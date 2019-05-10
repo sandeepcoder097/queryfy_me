b940ac3033cbe3c9e60609ad2e5e57747a759340
SANDEEP KUMAR SHARMA <31854379+sandeepcoder097@users.noreply.github.com>
2019-05-11 00:16:23 +0530

    Update email.py

--- python handle/email.py
+++ python handle/email.py
@@ -1,7 +1,7 @@
 import smtplib 
 s = smtplib.SMTP('smtp.gmail.com', 587) 
 s.starttls() 
-s.login("rockeythegreat.1@gmail.com", "Gmail@2018") 
+s.login("rockeythegreat.1@gmail.com", "P******d") 
 message = "Message_you_need_to_send"
 s.sendmail("rockeythegreat.1@gmail.com", "gupta.rajiv0703@gmail.com", message) 
 s.quit() 
